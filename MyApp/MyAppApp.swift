53239b3eb9880250ff7fcc8151c05c7a6ffdfc03
Hilal Kahraman <hllkhrmn@hotmail.com>
2026-06-26 22:50:42 +0300

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Hilal Kahraman on 26.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
